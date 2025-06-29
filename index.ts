cfc24c1ad1a3f9ba2915a6b58d7bc903f146c4db
Kreliannn <krelianquimson@gmail.com>
2025-06-30 06:19:40 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express,{ Request, Response } from 'express';
+import mongoose from 'mongoose';
+import routes from "./routes/route"
+import cors from "cors"
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongodb_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/barracks';
+  
+app.use(express.json());
+app.use(cors()); 
+app.use(routes)
+
+mongoose.connect(mongodb_uri)
+
+app.get('/', async (request: Request, response: Response) => {
+  response.send("working...........")
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
